da5e5f7feac9a38d8e382e31530c977c317672ae
fazlikziyaev <fazliddin2002.15@gmail.com>
2024-04-02 13:50:40 +0500

    Initial Commit

---
+++ FreeWeatherUpdate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeWeatherUpdate
+//
+//  Created by Fazlik Ziyaev on 02/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
