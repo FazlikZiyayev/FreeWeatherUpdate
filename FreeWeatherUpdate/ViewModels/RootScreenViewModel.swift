2329ca2d20fb6b96d4a6d88d89d895645b0cf2f0
fazlikziyaev <fazliddin2002.15@gmail.com>
2024-04-05 01:51:11 +0500

    adding country name label is done

--- FreeWeatherUpdate/ViewModels/RootScreenViewModel.swift
+++ FreeWeatherUpdate/ViewModels/RootScreenViewModel.swift
@@ -6,14 +6,64 @@
 //
 
 import Foundation
+import Combine
 
 
 protocol RootScreenViewModelProtocol {
+    var data: CurrentWeather? { get set }
     
+    var currentWeather: CurrentWeather? { get }
+    var currentWeatherPublisher: Published<CurrentWeather?>.Publisher { get }
+    
+    func fetch_currentWeather()
 }
 
 
 
 class RootScreenViewModel: RootScreenViewModelProtocol {
-    var currentWeather: CurrentWeather? = nil
+    var data: CurrentWeather?
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    @Published var currentWeather: CurrentWeather? = nil
+    var currentWeatherPublisher: Published<CurrentWeather?>.Publisher { $currentWeather }
+    
+    
+    func fetch_currentWeather() {
+        let urlStr = "\(NetworkConstants.shared.baseUrl)/data/2.5/weather?q=Uzbekistan&appid=\(NetworkConstants.shared.apiKey)&units=metric"
+        guard let safeUrl = URL(string: urlStr) else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTaskPublisher(for: safeUrl)
+            .tryMap { data, response -> Data in
+                guard let response = response as? HTTPURLResponse,
+                   (200...299).contains(response.statusCode) else
+                {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return data
+            }
+            .decode(type: CurrentWeather.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+            .sink(
+                //. completion when the publisher completes with failure or no error
+                receiveCompletion: { status in
+                    switch status {
+                    case .finished:
+                        print("Completed")
+                        break
+                    case .failure(let error):
+                        print("Receiver error \(error)")
+                        break
+                    }
+                },
+                // receive the data
+                receiveValue: { [weak self] weather in
+                    self?.currentWeather = weather
+                }
+            )
+            .store(in: &cancellables)
+    }
 }
--- FreeWeatherUpdate/Views/ViewControllers/RootScreenViewController.swift
+++ FreeWeatherUpdate/Views/ViewControllers/RootScreenViewController.swift
@@ -6,20 +6,74 @@
 //
 
 import UIKit
+import Combine
 
 
 class RootScreenViewController: UIViewController {
     
     let model: RootScreenViewModelProtocol = RootScreenViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
+    private lazy var countryNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        
+        return label
+    }()
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setup_uiComponents()
+        bind_components()
+        
+        model.fetch_currentWeather()
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    
+    
+    func bind_components() {
+        bind_currentWeatherPublisher()
+    }
+    
+    
+    func setup_uiComponents() {
+        setup_countryNameLabel()
+    }
+    
+    
+    
+    func bind_currentWeatherPublisher() {
+        model.currentWeatherPublisher
+            .receive(on: RunLoop.main)
+            .sink { completion in
+            } receiveValue: { [weak self] weather in
+                if let safeWeather = weather {
+                    self?.countryNameLabel.text = weather?.name
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+
+extension RootScreenViewController {
+    func setup_countryNameLabel() {
+        view.addSubview(countryNameLabel)
+        
+        NSLayoutConstraint.activate([
+            countryNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            countryNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                 constant: 30)
+        ])
     }
 }
